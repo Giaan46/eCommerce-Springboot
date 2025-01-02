7fe8047adc9f95d9477a93abb121e83df5830683
Gianluca <gianlucamaida2022@gmail.com>
2025-01-02 22:11:44 +0100

    anotaciones de la clase usuario y producto para la base de datos

--- src/main/java/com/ecommerce/demo/model/Producto.java
+++ src/main/java/com/ecommerce/demo/model/Producto.java
@@ -1,7 +1,18 @@
 package com.ecommerce.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String nombre;
 	private String secripcion;
@@ -9,8 +20,12 @@ public class Producto {
 	private double precio;
 	private int cantidad;
 	
-	
-	public Producto(int id, String nombre, String secripcion, String imagen, double precio, int cantidad) {
+	@ManyToOne
+	private Usuario usuario;
+
+
+	public Producto(int id, String nombre, String secripcion, String imagen, double precio, int cantidad,
+			Usuario usuario) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -18,51 +33,72 @@ public class Producto {
 		this.imagen = imagen;
 		this.precio = precio;
 		this.cantidad = cantidad;
+		this.usuario = usuario;
 	}
+
 	public Producto() {
-		
+
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getSecripcion() {
 		return secripcion;
 	}
+
 	public void setSecripcion(String secripcion) {
 		this.secripcion = secripcion;
 	}
+
 	public String getImagen() {
 		return imagen;
 	}
+
 	public void setImagen(String imagen) {
 		this.imagen = imagen;
 	}
+
 	public double getPrecio() {
 		return precio;
 	}
+
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
+
 	public int getCantidad() {
 		return cantidad;
 	}
+
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 	@Override
 	public String toString() {
 		return "Producto [id=" + id + ", nombre=" + nombre + ", secripcion=" + secripcion + ", imagen=" + imagen
 				+ ", precio=" + precio + ", cantidad=" + cantidad + "]";
 	}
-	
 
 }
--- src/main/java/com/ecommerce/demo/model/Usuario.java
+++ src/main/java/com/ecommerce/demo/model/Usuario.java
@@ -1,6 +1,21 @@
 package com.ecommerce.demo.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 
 	private int id;
 	private String nombre;
@@ -10,7 +25,8 @@ public class Usuario {
 	private String telefono;
 	private String tipo;
 	private String password;
-	
+	@OneToMany(mappedBy =  "usuarios")
+	private List<Producto> productos;
 	
 	public Usuario(int id, String nombre, String username, String email, String direccion, String telefono, String tipo,
 			String password) {
@@ -108,6 +124,16 @@ public class Usuario {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
 	}
 	
 	
