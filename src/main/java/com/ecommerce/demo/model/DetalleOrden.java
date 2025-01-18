78f17817352d39ec71623b6f418990fe08fe5e0e
Gianluca <gianlucamaida2022@gmail.com>
2025-01-18 03:46:30 +0100

    retoque del properties

--- src/main/java/com/ecommerce/demo/model/DetalleOrden.java
+++ src/main/java/com/ecommerce/demo/model/DetalleOrden.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -23,6 +24,7 @@ public class DetalleOrden {
 
 	@OneToOne
 	private Orden orden;
+	@ManyToOne
 	private Producto producto;
 
 	public DetalleOrden() {
